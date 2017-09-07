6b8ab164de037c400ab7edf708e0cb2708b12df5
John Seubert <john.seubert@personalcapital.com>
2017-09-06 18:32:20 -0700

    Add Firebase support

--- NotBaseballSigns/AppDelegate.m
+++ NotBaseballSigns/AppDelegate.m
@@ -9,6 +9,7 @@
 #import "AppDelegate.h"
 
 #import "YourNameViewController.h"
+#import "Firebase.h"
 
 @interface AppDelegate ()
 
@@ -19,6 +20,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   //  [Fabric with:@[[Crashlytics class]]];
+    [FIRApp configure];
     
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor whiteColor];
@@ -29,8 +31,21 @@
     
     UINavigationController *navcontroller = [[UINavigationController alloc] initWithRootViewController:[[YourNameViewController alloc] init]];
     self.window.rootViewController = navcontroller;
+    
+    [self registerForPushNotifications];
+    
     return YES;
-    return YES;
+}
+
+- (void)registerForPushNotifications {
+
+        // For iOS 10 display notification (sent via APNS)
+        [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+        UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+        [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            [[UIApplication sharedApplication] registerForRemoteNotifications];
+        }];
+
 }
 
 
@@ -60,5 +75,18 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+
+
+
+
+
+// The method will be called on the delegate only if the application is in the foreground. If the method is not implemented or the handler is not called in a timely manner then the notification will not be presented. The application can choose to have the notification presented as a sound, badge, alert and/or in the notification list. This decision should be based on whether the information in the notification is otherwise visible to the user.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler {
+    
+}
+// The method will be called on the delegate when the user responded to the notification by opening the application, dismissing the notification or choosing a UNNotificationAction. The delegate must be set before the application returns from applicationDidFinishLaunching:.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler  {
+    
+}
 
 @end
